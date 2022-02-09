a0db63709680bb81bd68be400c34f639064abf47
bombcar <github@bombcar.com>
2022-02-09 17:05:10 -0600

    versioning

--- src/main/java/com/github/lunatrius/ingameinfo/reference/Reference.java
+++ src/main/java/com/github/lunatrius/ingameinfo/reference/Reference.java
@@ -4,10 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Reference {
-    public static final String MODID = "GRADLETOKEN_MODID";
-    public static final String NAME = "GRADLETOKEN_MODNAME";
+    public static final String MODID = "InGameInfoXML";
+    public static final String NAME = "InGame Info XML";
     public static final String VERSION = "GRADLETOKEN_VERSION";
-    public static final String DEPENDENCIES = "after:LunatriusCore;";
+    public static final String DEPENDENCIES = "required-after:LunatriusCore;";
     public static final String PROXY_SERVER = "com.github.lunatrius.ingameinfo.proxy.ServerProxy";
     public static final String PROXY_CLIENT = "com.github.lunatrius.ingameinfo.proxy.ClientProxy";
     public static final String GUI_FACTORY = "com.github.lunatrius.ingameinfo.client.gui.GuiFactory";
